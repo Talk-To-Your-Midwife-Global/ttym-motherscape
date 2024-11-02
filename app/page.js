fbc93c025eacd792f9c807616f8d5d41578972c3
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-02 12:47:53 +0000

    feat: splash screen

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen items-center justify-center">
-      <Image src="/icons/logo-svg.svg" alt="logo" width={500} height={200} />
-      <div className="mt-4">
-        <p>Coming soon...</p>
-      </div>
-    </div>
+    <section className="flex flex-col h-screen w-screen items-center justify-center">
+     
+    </section>
   );
 }
