f4c04439050f709f35cc75dc0434808451823772
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-08 22:08:28 +0000

    style: add animations and fix ui dimensions

--- app/questions/page.js
+++ app/questions/page.js
@@ -5,12 +5,12 @@ import {IconButton } from "../components"
 export default function Page() {
     return (
         <section>
-            <Image className="relative left-2 mt-[20%]" src={character} alt="midwife with a tab in hand" />
+            <Image className="relative left-2 " src={character} alt="midwife with a tab in hand" />
             <div className="px-[20px]">
                 <h2 className="text-mainText font-medium text-xl">Hi there! Eager to get started?</h2>
                 <p className="text-subText">Let&apos;s set up your profile to help us get a better prediction.</p>
             </div>
-            <div className="fixed bottom-20 w-full flex items-center justify-center">
+            <div className="relative -bottom-10 w-full flex items-center justify-center">
                 <IconButton onClick={undefined} href="/questions/1" text="Get Started" icon="iconify lucide--arrow-right" />
             </div>
         </section>
