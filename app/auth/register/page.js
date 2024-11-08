d60d3e48b0b4d395ab9db211a4c289eed31dac6f
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-08 10:06:40 +0000

    fix: localstorage data fetching err

--- app/auth/register/page.js
+++ app/auth/register/page.js
@@ -9,16 +9,24 @@ import { SignUpForm } from "../components";
 
 export default function Page() {
     const [state, action] = useActionState(signup, undefined)
+    const [userRoute, setUserRoute] = useState('');
     const router = useRouter()
     console.log(state)
     if (state?.success) {
         router.push('/auth/signIn/')
     }
+    const getUserRouteFromLocalStorage = () => {
+        JSON.parse(localStorage.getItem('userType'))
+    }
+
+    useEffect(() => {
+        setUserRoute(getUserRouteFromLocalStorage())
+    }, [])
     return (
         <section>
             <header className="flex">
                 <nav className="my-5">
-                    <Link href={`/onboarding/${localStorage.getItem('userType')}/3`} className="w-12 h-12 rounded-full flex justify-center items-center">
+                    <Link href={`/onboarding/${userRoute}/3`} className="w-12 h-12 rounded-full flex justify-center items-center">
                         <span className="iconify material-symbols-light--chevron-left-rounded font-medium text-3xl text-[#000]"></span>
                     </Link>
                 </nav>
