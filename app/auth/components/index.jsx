a7942f13026644c44cf72b4958de9c85a7d56184
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-11 17:59:13 +0000

    fix: sign in link not working

--- app/auth/components/index.jsx
+++ app/auth/components/index.jsx
@@ -11,9 +11,23 @@ import apple from "../../../public/images/apple.svg"
 export function HelpCenterLinks({signIn=true}) {
     return (
         <section className="text-primaryText text-center text-sm font-medium">
-            <p className="mb-5">Already have an account? <Link className="text-primaryColor" href={`/auth/${signIn ? 'sigIn' : 'register'}`}>{signIn ? 'sigIn' : 'signup'}</Link> </p>
-            <p>Need help? Visit our <Link className="text-primaryColor" href="/help">help center</Link></p> {/** TODO:Create help center page */}
+            <p className="mb-5">Already have an account?
+                <Link className="text-primaryColor" href={`/auth/${signIn ? 'signIn' : 'register'}`}>{signIn ? 'signIn' : 'signup'}</Link>
+            </p>
+            <p>Need help? Visit our
+                <Link className="text-primaryColor" href="/help">help center</Link></p> {/** TODO:Create help center page */}
         </section>
+    )
+}
+
+export function AuthNav({backUrl}) {
+    return (
+        <nav className="my-5">
+            <Link href={backUrl} className="w-12 h-12 rounded-full flex justify-center
+                    items-center">
+                <span className="iconify material-symbols-light--chevron-left-rounded font-medium text-3xl text-[#000]"></span>
+            </Link>
+        </nav>
     )
 }
 
