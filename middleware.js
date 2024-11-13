d3ce475ee4306d1b0cc6200324f0e34efcd78688
Prince Asiedu <princekofasiedu@gmail.com>
2024-11-13 20:11:59 +0000

    fix: update protected paths

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const config = {
     // match: ['/', '/onboarding', '/register', '/signIn']
-    matcher: ["/questions", '/dashboard']
+    matcher: ["/questions/:path*", '/dashboard/:path*']
 }
 
 export function middleware(request) {
