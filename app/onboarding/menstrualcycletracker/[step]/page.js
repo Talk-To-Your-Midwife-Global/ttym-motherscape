524a4262f795c9dff95a27cfc73d33cf08c20a37
Prince Asiedu <princekofasiedu@gmail.com>
2024-12-04 16:15:26 +0000

    feat: pregnancy onboarding complete

--- app/onboarding/menstrualcycletracker/[step]/page.js
+++ app/onboarding/menstrualcycletracker/[step]/page.js
@@ -1,6 +1,5 @@
-
 import Image from "next/image";
-import { StepOne, StepTwo, StepThree } from "../../components"
+import {StepOne, StepTwo, StepThree} from "../../components"
 import rafiki from "../../../../public/images/rafiki.svg"
 import cuate from "../../../../public/images/cuate.svg"
 import pana from "../../../../public/images/pana.svg"
@@ -8,15 +7,26 @@ import pana from "../../../../public/images/pana.svg"
 export default async function Page({params}) {
     const step = await params;
     const view = {
-        '1':<StepOne userType="menstrualcycletracker">
-                <Image src={rafiki} className="" alt="image of a woman at different stages of pregnancy" />
-            </StepOne>,
-        '2':<StepTwo userType="menstrualcycletracker"> 
-                <Image src={cuate} className="" alt="three women talking" />
-            </StepTwo>, 
-        '3':<StepThree> 
-                <Image src={pana} className="" alt="three women talking" />
-            </StepThree>,
+        '1': <StepOne
+            userType="menstrualcycletracker"
+            title={"Track Your Cycle with Confidence!"}
+            description={"Stay on top of your period, symptoms, and mood with personalized insights tailored to your unique cycle."}
+        >
+            <Image src={rafiki} className="" alt="image of a woman at different stages of pregnancy"/>
+        </StepOne>,
+        '2': <StepTwo
+            userType="menstrualcycletracker"
+            title={"A Community to Rely on"}
+            description={"A community forum to share your journey or listen to success stories"}
+        >
+            <Image src={cuate} className="" alt="three women talking"/>
+        </StepTwo>,
+        '3': <StepThree
+            title={"Want to talk to a midwife or a doctor"}
+            description={"Get in touch with our midwives and doctors to help better your monthly flow"}
+        >
+            <Image src={pana} className="" alt="three women talking"/>
+        </StepThree>,
     };
 
     return (
--- app/onboarding/pregnancytracker/[step]/page.js
+++ app/onboarding/pregnancytracker/[step]/page.js
@@ -1,21 +1,35 @@
 import Image from "next/image"
-import { StepOne, StepTwo, StepThree } from "../../components"
-import rafiki from "../../../../public/images/rafiki.svg"
-import cuate from "../../../../public/images/cuate.svg"
-import pana from "../../../../public/images/pana.svg"
+import {StepOne, StepTwo, StepThree} from "../../components"
+import rafiki from "../../../../public/images/pregnant/rafiki.svg"
+import cuate from "../../../../public/images/pregnant/cuate.svg"
+import pana from "../../../../public/images/pregnant/pana.svg"
 
 export default async function Page({params}) {
     const step = await params;
     const view = {
-        '1':<StepOne userType="pregnancytracker">
-                <Image src={rafiki} className="" alt="image of a woman at different stages of pregnancy" />
-            </StepOne>,
-        '2':<StepTwo userType="pregnancytracker"> 
-                <Image src={cuate} className="" alt="three women talking" />
-            </StepTwo>, 
-        '3':<StepThree> 
-                <Image src={pana} className="" alt="three women talking" />
-            </StepThree>,
+        '1': <StepOne
+            userType="pregnancytracker"
+            title={"Welcome to Your Pregnancy Journey"}
+            description={"Track your pregnancy, stay informed, and connect with professionals."}
+
+        >
+            <Image src={pana} className="" alt="image of a woman at different stages of pregnancy"/>
+        </StepOne>,
+        '2': <StepTwo
+            userType="pregnancytracker"
+            title={"Your Journey, Your Way!"}
+            description={"Set your due date, track milestones, and receive insights tailored to your unique pregnancy journey. Start personalizing your experience now!"}
+
+        >
+            <Image src={rafiki} className="" alt="three women talking"/>
+        </StepTwo>,
+        '3': <StepThree
+            title={"You’re Not Alone!"}
+            description={"Join a supportive community of moms-to-be and connect with experts who can answer your questions and guide you.w"}
+
+        >
+            <Image src={cuate} className="" alt="three women talking"/>
+        </StepThree>,
     };
     return (
         view[step.step]
