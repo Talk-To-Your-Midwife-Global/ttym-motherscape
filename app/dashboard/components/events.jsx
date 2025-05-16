2bcf4e8374a8f2ced411c0fa7b6f620eac3852d1
Prince Asiedu <princekofasiedu@gmail.com>
2025-05-16 04:42:01 +0000

    misc

--- app/dashboard/components/events.jsx
+++ app/dashboard/components/events.jsx
@@ -19,26 +19,34 @@ export function Events({accessToken}) {
 
     if (isLoading) return <div>Loading events...</div>
 
-    return (<section className={"px-5 my-10 h-[300px]"}>
-        <header>
-            <div className={"flex justify-between"}>
-                <h2 className={"text-primaryText font-bold text-xl"}>Upcoming Events</h2>
-                {/*<Link href={"/"}>See More</Link> /!* TODO: use the right link*!/*/}
-            </div>
-            <p className={`${montserrat.className} text-subText`}>Live Health Talk events by your Global midwife</p>
-        </header>
-        <section className={"mt-4 flex items-center px-4 carousel  overflow-x-auto scroll-smooth space-x-4 p-4"}>
-            {/* Make it a carousel TODO:Make it a carousel*/}
-            {data && data?.map(event => {
-                return (<div key={event.id}
-                             className={"flex justify-between overflow-hidden gap-3 bg-white carousel-item rounded-2xl px-5 py-4 h-[250px] flex-shrink-0 w-52"}>
-                    <Image src={`${PUBLICHOSTNAME}${event.thumbnail}`} width={200}
-                           height={200} alt={`event`}/>
-                </div>)
-            })}
-            {data.length < 1 && <div>
-                <SmallEmptyState text={"No events at the moment"}/>
-            </div>}
-        </section>
-    </section>)
+    return (
+        <section className={"px-5 my-10 h-[300px]"}>
+            <header>
+                <div className={"flex justify-between"}>
+                    <h2 className={"text-primaryText font-bold text-xl"}>Upcoming Events</h2>
+                    {/*<Link href={"/"}>See More</Link> /!* TODO: use the right link*!/*/}
+                </div>
+                <p className={`${montserrat.className} text-subText`}>Live Health Talk events by your Global midwife</p>
+            </header>
+            <section className={"mt-4 flex items-center px-4 carousel  overflow-x-auto scroll-smooth space-x-4 p-4"}>
+                {/* Make it a carousel TODO:Make it a carousel*/}
+                {data && data?.map(event => {
+                        return (
+                            <div key={event.id}
+                                 className="flex justify-between overflow-hidden gap-3 bg-white carousel-item rounded-2xl px-5 py-4 h-[250px]
+                             flex-shrink-0 w-52">
+                                <Image src={`${PUBLICHOSTNAME}${event.thumbnail}`} width={200} height={200} alt={`event`}/>
+                            </div>
+                        )
+                    }
+                )}
+
+                {
+                    data.length < 1 &&
+                    <div>
+                        <SmallEmptyState text={"No events at the moment"}/>
+                    </div>
+                }
+            </section>
+        </section>)
 }
