d9cd9280537bf8ca1317fa73bc670a1735eefe24
Prince Asiedu <princekofasiedu@gmail.com>
2025-07-11 18:47:04 +0000

    fix: disabled typo issue

--- app/_components/FormSelect.jsx
+++ app/_components/FormSelect.jsx
@@ -1,12 +1,12 @@
 import {convertSpacedStringToDashDelimited} from "@/app/_lib/functions";
 
-export function FormSelect({label, options, disbaled = true}) {
+export function FormSelect({label, options, disabled = true}) {
     const name = convertSpacedStringToDashDelimited(label);
 
     return (
         <div className="flex flex-col gap-2 my-2">
             <label className="text-[#999999] text-sm capitalize">Language</label>
-            <select name={name} disabled={disbaled}
+            <select name={name} disabled={disabled}
                     className="border-2 border-[#999999] rounded-md p-2 placeholder-text-gray-500">
                 {/*<option value="#">--No Selection --</option>*/}
                 {/*Expects an object: {value, label}*/}
