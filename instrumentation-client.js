fc55cb4ccf3d60b50973dc3bf55112c5809947b7
Prince Asiedu <princekofasiedu@gmail.com>
2025-09-08 15:17:52 +0000

    feat: analytics made to work only in prod

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -1,17 +1,19 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-	api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-	defaults: "2025-05-24",
-	autocapture: {
-		// Disable Copy/Cut Analytics
-		capture_copied_text: false,
+if (process.env.NODE_ENV === "production") {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        defaults: "2025-05-24",
+        autocapture: {
+            // Disable Copy/Cut Analytics
+            capture_copied_text: false,
 
-		// DOM Event List
-		// dom_event_allowlist: ["click", "submit"],
+            // DOM Event List
+            // dom_event_allowlist: ["click", "submit"],
 
-		element_allowlist: ["button", "form", "a"],
+            element_allowlist: ["button", "form", "a"],
 
-		
-	}
-});
+
+        }
+    });
+}
