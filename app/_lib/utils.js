85d84d68709762ac34cfbe5131b5d886748afaba
Prince Asiedu <princekofasiedu@gmail.com>
2025-09-11 22:05:12 +0000

    debug: viewing logs

--- app/_lib/utils.js
+++ app/_lib/utils.js
@@ -18,12 +18,12 @@ export function cn(...inputs) {
 
 //
 export function Log(...inputs) {
-    if (typeof window === "undefined") {
-        if (process.env.environment !== "production") {
+    // if (typeof window === "undefined") {
+    //     if (process.env.environment !== "production") {
             console.log(...inputs);
-        }
+        // }
     }
-    if (process.env.NEXT_PUBLIC_ENVIRONMENT !== "production") {
-        console.log(...inputs);
-    }
+    // if (process.env.NEXT_PUBLIC_ENVIRONMENT !== "production") {
+    //     console.log(...inputs);
+    // }
 }
