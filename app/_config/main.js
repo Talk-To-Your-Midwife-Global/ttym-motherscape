36af52299c63f3445d914afca782d3d82a32c270
Prince Asiedu <princekofasiedu@gmail.com>
2025-09-16 11:56:01 +0000

    fix: route errors

--- app/_config/main.js
+++ app/_config/main.js
@@ -1,2 +1,4 @@
 export const HOSTNAME_URI = `${process.env.environment === 'development' ? 'http' : 'https'}://${process.env.HOSTNAME_URI}`;
 export const PUBLICHOSTNAME = `${process.env.NEXT_PUBLIC_ENVIRONMENT === 'development' ? 'http' : 'https'}://${process.env.NEXT_PUBLIC_HOSTNAME}`;
+export const currentRoute = `${process.env.environment === 'development' ? 'http://localhost:3000' : 'https://app.talktoyourmidwife.com'}`;
+
